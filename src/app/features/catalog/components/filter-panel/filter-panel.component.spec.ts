356260d3382c8060cece3561e1fef683e1183caf
Erick Ricardo <edck030303@gmail.com>
2025-10-22 17:35:33 -0300

    Cria WishlistComponent e corrige ProductCardComponent

--- src/app/features/catalog/components/filter-panel/filter-panel.component.spec.ts
+++ src/app/features/catalog/components/filter-panel/filter-panel.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FilterPanel } from './filter-panel';
+import { FilterPanel } from './filter-panel.component';
 
 describe('FilterPanel', () => {
   let component: FilterPanel;
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,5 @@
+exports: [
+  HeaderComponent,
+  FooterComponent,
+  ButtonComponent
+]
